c321d4d28cd05871705927d481ed2ebc26a21dc8
the static collective <joyvolcanomusic@gmail.com>
2026-08-31 17:18:08 -0500

    feat: export read-only host adapter surface

--- src/loadout/dev/__init__.py
+++ src/loadout/dev/__init__.py
@@ -1,5 +1,7 @@
 from loadout.dev.adapters import Adapter, FakeAdapter
 from loadout.dev.compiler import compile_world
+from loadout.dev.live_git import resolve_current_organ_from_git
+from loadout.dev.local_git import LocalGitReadAdapter
 from loadout.dev.membrane import invoke_effect
 from loadout.dev.model import (
     AdapterBody,
@@ -13,6 +15,7 @@ from loadout.dev.model import (
     OwnerGate,
     RefusalReason,
     WorkflowEvent,
+    parameter_map,
 )
 from loadout.dev.workflow import (
     DEV_DEBUG,
@@ -27,6 +30,7 @@ from loadout.dev.workflow import (
 __all__ = [
     "Adapter", "AdapterBody", "CapabilityRequest", "CapabilitySpec", "CompileReceipt", "CompileRequest",
     "DEV_DEBUG", "DEV_DOCS", "DEV_IMPLEMENT", "DEV_LAND", "DEV_REVIEW",
-    "EffectClass", "EffectIntent", "EffectReceipt", "FakeAdapter", "OwnerGate", "RefusalReason",
-    "WorkflowEvent", "compile_world", "invoke_effect", "start_workflow", "transition",
+    "EffectClass", "EffectIntent", "EffectReceipt", "FakeAdapter", "LocalGitReadAdapter", "OwnerGate",
+    "RefusalReason", "WorkflowEvent", "compile_world", "invoke_effect", "parameter_map",
+    "resolve_current_organ_from_git", "start_workflow", "transition",
 ]
