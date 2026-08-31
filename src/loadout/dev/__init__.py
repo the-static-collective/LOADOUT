210c82de1319c135c1d8348b01c25316d887e3e0
the static collective <joyvolcanomusic@gmail.com>
2026-08-31 16:57:25 -0500

    feat: export merge formation analyzer

--- src/loadout/dev/__init__.py
+++ src/loadout/dev/__init__.py
@@ -1,6 +1,11 @@
 from loadout.dev.adapters import Adapter, FakeAdapter
 from loadout.dev.compiler import compile_world
 from loadout.dev.membrane import invoke_effect
+from loadout.dev.merge_formation import (
+    MergeFormationInputError,
+    analyze_merge_formation,
+    render_merge_formation_receipt,
+)
 from loadout.dev.model import (
     AdapterBody,
     CapabilityRequest,
@@ -29,4 +34,5 @@ __all__ = [
     "DEV_DEBUG", "DEV_DOCS", "DEV_IMPLEMENT", "DEV_LAND", "DEV_REVIEW",
     "EffectClass", "EffectIntent", "EffectReceipt", "FakeAdapter", "OwnerGate", "RefusalReason",
     "WorkflowEvent", "compile_world", "invoke_effect", "start_workflow", "transition",
+    "MergeFormationInputError", "analyze_merge_formation", "render_merge_formation_receipt",
 ]
